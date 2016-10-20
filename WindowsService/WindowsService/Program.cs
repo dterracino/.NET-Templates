22ce38995c8bbdfa770cbbc9b9eec2d0ad7c5f1a
Fabian Lenz <fale1de@resco.local>
2016-10-20 18:50:46 +0200

    Initial commit

---
+++ WindowsService/WindowsService/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.ServiceProcess;
+using Serilog;
+
+namespace WindowsService
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            Directory.SetCurrentDirectory(AppDomain.CurrentDomain.BaseDirectory);
+
+            Log.Logger = new LoggerConfiguration()
+             .ReadFrom.AppSettings()
+             .CreateLogger();
+
+            var service = new Service();
+
+            if (Environment.UserInteractive)
+            {
+                service.ConsoleStart(args);
+                Console.WriteLine("Press any key to stop program");
+                Console.Read();
+                service.ConsoleStop();
+            }
+            else
+            {
+                ServiceBase.Run(service);
+            }
+        }
+    }
+}
---
+++ WindowsService/WindowsService/Service.cs
@@ -0,0 +1,33 @@
+﻿using System.ServiceProcess;
+using Serilog;
+
+namespace WindowsService
+{
+    public partial class Service : ServiceBase
+    {
+        public Service()
+        {
+            InitializeComponent();
+        }
+
+        public void ConsoleStart(string[] args)
+        {
+            OnStart(args);
+        }
+
+        public void ConsoleStop()
+        {
+            OnStop();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            Log.Information("Starting up...");
+        }
+
+        protected override void OnStop()
+        {
+            Log.Information("Shutting down...");
+        }
+    }
+}
